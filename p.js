bedad48f5b6000f11968319ff7e832a3e77104ae
ug_lfa <im642176940@gmail.com>
2019-12-10 17:43:31 +0800

    tab to space

--- p.js
+++ p.js
@@ -1,103 +1,101 @@
-/*
-	Tiny promise, easy to understand
-*/
+/* Tiny promise, easy to understand */
 class P {
-	constructor(fn) {
-		this.value = void 0;
-		this.state = "pending";
-		this.resolves = [];
-		this.rejectes = [];
+  constructor(fn) {
+    this.value = void 0;
+    this.state = "pending";
+    this.resolves = [];
+    this.rejectes = [];
 
-		const resolve = data => {
-			if (data instanceof P) {
-				P.captureError.remove(data); // remove from unhaddlePromise list
-				return data.then(resolve, reject);
-			}
+    const resolve = data => {
+      if (data instanceof P) {
+        P.captureError.remove(data); // remove from unhaddlePromise list
+        return data.then(resolve, reject);
+      }
 
-			if (this.state !== "pending") return;
-			this.state = "resolved";
-			this.value = data;
-			this.resolves.forEach(fn => fn(this.value));
-		};
+      if (this.state !== "pending") return;
+      this.state = "resolved";
+      this.value = data;
+      this.resolves.forEach(fn => fn(this.value));
+    };
 
-		const reject = data => {
-			if (this.state !== "pending") return;
-			this.state = "rejected";
-			this.value = data;
-			this.rejectes.forEach(fn => fn(this.value));
-			P.captureError(this);
-		};
+    const reject = data => {
+      if (this.state !== "pending") return;
+      this.state = "rejected";
+      this.value = data;
+      this.rejectes.forEach(fn => fn(this.value));
+      P.captureError(this);
+    };
 
-		try {
-			return fn(resolve, reject); // Promise返回新实例，其他值返回new this
-		} catch (e) {
-			reject(`Error ${e.message}`);
-		}
-	}
+    try {
+      return fn(resolve, reject); // Promise返回新实例，其他值返回new this
+    } catch (e) {
+      reject(`Error ${e.message}`);
+    }
+  }
 
-	then(ok = P.transfer, fail = P.transfer) {
-		let _resolve, _reject;
-		const thenP = new P(function(resolve, reject) {
-			_resolve = resolve;
-			_reject = reject;
-		});
+  then(ok = P.transfer, fail = P.transfer) {
+    let _resolve, _reject;
+    const thenP = new P(function(resolve, reject) {
+      _resolve = resolve;
+      _reject = reject;
+    });
 
-		if (this.state === "resolved") {
-			asyncResolve(this.value, ok, _resolve, _reject);
-		}
+    if (this.state === "resolved") {
+      asyncResolve(this.value, ok, _resolve, _reject);
+    }
 
-		if (this.state === "rejected") {
-			const failResolve = fail === P.transfer ? _reject : _resolve;
-			asyncResolve(this.value, fail, failResolve, _reject);
-		}
+    if (this.state === "rejected") {
+      const failResolve = fail === P.transfer ? _reject : _resolve;
+      asyncResolve(this.value, fail, failResolve, _reject);
+    }
 
-		if (this.state === "pending") {
-			this.resolves.push(() => {
-				asyncResolve(this.value, ok, _resolve, _reject);
-			});
-			this.rejectes.push(() => {
-				const failResolve = fail === P.transfer ? _reject : _resolve;
-				asyncResolve(this.value, fail, failResolve, _reject);
-			});
-		}
+    if (this.state === "pending") {
+      this.resolves.push(() => {
+        asyncResolve(this.value, ok, _resolve, _reject);
+      });
+      this.rejectes.push(() => {
+        const failResolve = fail === P.transfer ? _reject : _resolve;
+        asyncResolve(this.value, fail, failResolve, _reject);
+      });
+    }
 
-		return thenP;
-	}
+    return thenP;
+  }
 
-	catch(haddle) {
-		return this.then(null, haddle);
-	}
+  catch(haddle) {
+    return this.then(null, haddle);
+  }
 
-	static resolve = value => new P(res => res(value));
+  static resolve = value => new P(res => res(value));
 
-	static reject = value => new P((res, rej) => rej(value));
+  static reject = value => new P((res, rej) => rej(value));
 
-	// pass origin value to thenPromise without haddler
-	static transfer = value => value;
+  // pass origin value to thenPromise without haddler
+  static transfer = value => value;
 
-	// mock notify Promise Error
-	static captureError = (() => {
-		const unhaddlePromises = [];
+  // mock notify Promise Error
+  static captureError = (() => {
+    const unhaddlePromises = [];
 
-		function captureError(promise) {
-			if (promise.rejectes.length) return;
-			unhaddlePromises.push(promise);
+    function captureError(promise) {
+      if (promise.rejectes.length) return;
+      unhaddlePromises.push(promise);
 
-			callInMacroTurn(() => {
-				unhaddlePromises.forEach(promise =>
-					console.error(`Uncaught Promise ${promise.value}`)
-				);
-				unhaddlePromises.length = 0;
-			});
-		}
+      callInMacroTurn(() => {
+        unhaddlePromises.forEach(promise =>
+          console.error(`Uncaught Promise ${promise.value}`)
+        );
+        unhaddlePromises.length = 0;
+      });
+    }
 
-		captureError.remove = promise => {
-			const index = unhaddlePromises.indexOf(promise);
-			unhaddlePromises.splice(index, 1);
-		};
+    captureError.remove = promise => {
+      const index = unhaddlePromises.indexOf(promise);
+      unhaddlePromises.splice(index, 1);
+    };
 
-		return captureError;
-	})();
+    return captureError;
+  })();
 }
 
 /**
@@ -105,15 +103,15 @@ class P {
  * @param {需要加入微任务的回调} cb
  */
 function callInMacroTurn(cb) {
-	const gob = (() => this)();
-	if (gob.process && process.nextTick) return process.nextTick(cb);
+  const gob = (() => this)();
+  if (gob.process && process.nextTick) return process.nextTick(cb);
 
-	const observer = new MutationObserver(cb);
-	const target = document.createElement("span");
-	observer.observe(target, { attributes: true });
-	target.setAttribute("dispatchMutation", Date.now());
+  const observer = new MutationObserver(cb);
+  const target = document.createElement("span");
+  observer.observe(target, { attributes: true });
+  target.setAttribute("dispatchMutation", Date.now());
 
-	return observer;
+  return observer;
 }
 
 /**
@@ -124,82 +122,82 @@ function callInMacroTurn(cb) {
  * @param {haddler执行出错时统一置错} reject
  */
 function asyncResolve(value, haddler, resolve, reject) {
-	callInMacroTurn(() => {
-		try {
-			resolve(haddler(value));
-		} catch (e) {
-			reject(`Error ${e.message}`);
-		}
-	});
+  callInMacroTurn(() => {
+    try {
+      resolve(haddler(value));
+    } catch (e) {
+      reject(`Error ${e.message}`);
+    }
+  });
 }
 
 /**
  * 测试脚本，检验手动实现与原生方法的区别
  */
 (function() {
-	const gob = (() => this)();
+  const gob = (() => this)();
 
-	t(P);
-	t(Promise);
+  t(P);
+  t(Promise);
 
-	function t(C) {
-		const sign = C.name;
-		const log = console.log.bind(console, sign);
-		const time = gob.performance
-			? () => performance.now()
-			: gob.process
-			? () => process.hrtime()[1]
-			: () => Date.now();
+  function t(C) {
+    const sign = C.name;
+    const log = console.log.bind(console, sign);
+    const time = gob.performance
+      ? () => performance.now()
+      : gob.process
+      ? () => process.hrtime()[1]
+      : () => Date.now();
 
-		(() => {
-			var t = time();
+    (() => {
+      var t = time();
 
-			C.reject(1234);
+      C.reject(1234);
 
-			new C((a, b) => {
-				const n = time();
-				log("new", n - t);
-				t = n;
-				a(124);
-			})
-				.then(res => {
-					const n = time();
-					log("then.1", res, n - t);
-					t = n;
-					throw new Error("111");
-				})
-				.catch(e => {
-					const n = time();
-					log("catch.1", n - t);
-					t = n;
-					return C.reject(222);
-				})
-				.catch(p => {
-					return new C((res, rej) => {
-						setTimeout(() => {
-							rej(p);
-						}, 1200);
-					});
-				})
-				.then()
-				.catch(e => {
-					const n = time();
-					log("catch.2", n - t);
-					t = n;
-					throw new Error("121");
-				});
+      new C((a, b) => {
+        const n = time();
+        log("new", n - t);
+        t = n;
+        a(124);
+      })
+        .then(res => {
+          const n = time();
+          log("then.1", res, n - t);
+          t = n;
+          throw new Error("111");
+        })
+        .catch(e => {
+          const n = time();
+          log("catch.1", n - t);
+          t = n;
+          return C.reject(222);
+        })
+        .catch(p => {
+          return new C((res, rej) => {
+            setTimeout(() => {
+              rej(p);
+            }, 1200);
+          });
+        })
+        .then()
+        .catch(e => {
+          const n = time();
+          log("catch.2", n - t);
+          t = n;
+          throw new Error("121");
+        });
 
-			(function() {
-				const n = time();
-				log("console", n - t);
-				t = n;
-			})();
+      (function() {
+        const n = time();
+        log("console", n - t);
+        t = n;
+      })();
 
-			setTimeout(() => {
-				const n = time();
-				log("timeout", n - t);
-				t = n;
-			});
-		})();
-	}
+      setTimeout(() => {
+        const n = time();
+        log("timeout", n - t);
+        t = n;
+      });
+    })();
+  }
 })();
